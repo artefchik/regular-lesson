18ed5083bf238ce9231a80f74e05a2599056f9cc
Artem Adeev <artemadeev42@gmail.com>
2023-10-31 12:23:47 +0300

    task1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
+function task1(url) {
+    // const reg = /^(https?:\/\/)?([\da-z\.-]+)\.([az\.]{2,6})([\/\w \.-]*)*\/?$/;
+    const reg = /(?:https?):\/\/(\w+:?\w*)?(\S+)(:\d+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
+    return reg.test(url);
+}
+
+
 
